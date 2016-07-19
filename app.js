1ad38d128993edc40f4e5358a72dec0cfebc732c
shehanmj <shehanmj@yahoo.com>
2016-07-19 12:29:54 -0500

    added play, pause and volume functionality

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-	$('#play').click(function () {
-		console.log("it works!");
+	var volume1 = document.getElementById('rngVolume').value;
+	
+	var song = document.getElementById("player"); 
+	
+	$('#rngVolume').on('change', function() {
+    $(song).prop("volume", this.value);
+});
+
+	$('#btnPlay').click(function () {
+		song.play();
+	});
+
+	$('#btnPause').click(function () {
+		song.pause();
 	});
 });
